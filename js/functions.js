495d4a5cccc8c6e9fdfc150bc510b8f10bd0a7d8
tamirandtom <tamirp@gmail.com>
2017-07-29 19:01:25 +0300

    initial site

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,22 @@
-$(document).ready(function() { 
+$(document).ready(function () {
 
-// your stuff here
+    $('.wl-carousel').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        prevArrow: '<div class="slick-prev wl-arrow"></div>',
+        nextArrow: '<div class="slick-next wl-arrow"></div>'
+        //   adaptiveHeight: true
+    });
+
+    
+    $( ".wl-download" ).on( "click", function(e) {
+    // console.log();
+     e.preventDefault();  //stop the browser from following
+    window.location.href = $(".slick-active").attr('data-link');
+
+});
+
 
 });
